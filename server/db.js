9ed6beed39d35f53f12ddabe70297f16f5156609
Joker <srivartkovid@gmail.com>
2024-08-17 09:53:05 +0530

    feat: db connections

---
+++ server/db.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require("mongodb");
+ 
+var _db;
+ 
+module.exports = {
+  connectToServer: async function  () {
+    console.log("Connecting to MongoDB");
+    const client = new MongoClient(process.env.MONGO_URI);
+    _db = client.db('ignition');
+    console.log(_db);
+    _admindb = client.db('admin');
+
+  },
+ 
+  getDb: function () {
+    return _db;
+  
+  },
+  getAdminDb : function(){
+    return _admindb;
+  }
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5001;
+require('dotenv').config();
 
 app.use(express.json());
 
